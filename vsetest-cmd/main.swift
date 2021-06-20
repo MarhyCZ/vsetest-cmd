a4687732372e2fe6dacbf206d478f8e373ab6347
Michal Marhan <marhanmichal@gmail.com>
2021-06-20 13:24:07 +0200

    Refactoring / Logic for handling answering

--- vsetest-cmd/main.swift
+++ vsetest-cmd/main.swift
@@ -14,13 +14,13 @@ struct Answer: Hashable {
 
 struct Question: Hashable {
     let title: String
-    let answers:[Answer]
+    let answers: [Answer]
     let correctStreak: Int? = nil
     
 }
 
-// fresh means not yet played
 struct QuestionSet {
+    // fresh means not yet played
     var fresh = Set<Question>()
     var correct = Set<Question>()
     var mistaken = Set<Question>()
@@ -28,7 +28,7 @@ struct QuestionSet {
     var freshCount: Int {
         return fresh.count
     }
-    var mistakeCount: Int {
+    var mistakenCount: Int {
         return mistaken.count
     }
     var correctCount: Int {
@@ -42,109 +42,129 @@ struct QuestionSet {
     }
 }
 
-func loadQuestionsFile() throws -> [Question] {
-    let fileURL = URL.init(fileURLWithPath: "/Users/michal/Downloads/Pravo 2014 - UTF.txt")
-    
-    let fileContents = try String(contentsOf: fileURL)
-    let textLines = fileContents.components(separatedBy: .newlines)
-    return parseQuestions(with: textLines)
-    
-    //    let handle = FileHandle.standardInput
-    //    for try await line in handle.bytes.lines {
-    //        print(line)
-    //    }
-}
-
-func parseQuestions(with textLines: [String]) -> [Question] {
-    var questions = [Question]()
-    var currQuestion = ""
-    var currAnswers = [Answer]()
-    
-    textLines.forEach { line in
-        if (line.isEmpty) {return}
-        // If question ended (new question title detected), create new one
-        let shouldEndQuestion = !(line.first == "+" || line.first == "-")
-        if (shouldEndQuestion) {
-            questions.append(Question(title: currQuestion, answers: currAnswers))
-            currAnswers.removeAll()
-            currQuestion = line
-        } else {
-            var strippedLine = line
-            let isCorrect = strippedLine.removeFirst() == "+"
-            // Remove spaces at the beginning
-            strippedLine = strippedLine.trimmingCharacters(in: .whitespaces)
-            currAnswers.append(Answer(text: strippedLine, isCorrect: isCorrect))
-        }
+class QuestionsLoader {
+    static func loadQuestionsFile() throws -> [Question] {
+        let fileURL = URL.init(fileURLWithPath: "/Users/michal/Downloads/Pravo 2014 - UTF.txt")
+        
+        let fileContents = try String(contentsOf: fileURL)
+        let textLines = fileContents.components(separatedBy: .newlines)
+        return parseQuestions(with: textLines)
+        
+        //    let handle = FileHandle.standardInput
+        //    for try await line in handle.bytes.lines {
+        //        print(line)
+        //    }
     }
-    questions.append(Question(title: currQuestion, answers: currAnswers)) // Add last question
-    questions.removeFirst() // First is empty
-    return questions
+    
+    static func parseQuestions(with textLines: [String]) -> [Question] {
+        var questions = [Question]()
+        var currQuestion = ""
+        var currAnswers = [Answer]()
+        
+        textLines.forEach { line in
+            if (line.isEmpty) {return}
+            // If question ended (new question title detected), create new one
+            let shouldEndQuestion = !(line.first == "+" || line.first == "-")
+            if (shouldEndQuestion) {
+                questions.append(Question(title: currQuestion, answers: currAnswers))
+                currAnswers.removeAll()
+                currQuestion = line
+            } else {
+                var strippedLine = line
+                let isCorrect = strippedLine.removeFirst() == "+"
+                // Remove spaces at the beginning
+                strippedLine = strippedLine.trimmingCharacters(in: .whitespaces)
+                currAnswers.append(Answer(text: strippedLine, isCorrect: isCorrect))
+            }
+        }
+        questions.append(Question(title: currQuestion, answers: currAnswers)) // Add last question
+        questions.removeFirst() // First question is always empty
+        return questions
+    }
 }
 
 class Quiz {
     private var questions: QuestionSet
-    private var currentQuestion: Question
+    private(set) public var currentQuestion: Question
     // Number of questions before trying one of the mistaken
     private var nextTry: Int?
+    private(set) public var mistakes: Int
     // Evidovat chyby
     // Chybna odpoved bude potrebovat 3x rict spravne otazku
     // bude potreba chybne otazky nejakou random logikou ukazovat drive
     // Zamichat odpovedi
     init(using questions: [Question]) {
         self.nextTry = nil
+        self.mistakes = 0
         self.questions = QuestionSet()
         self.questions.fresh = Set(questions)
         self.currentQuestion = Question(title: "", answers: [Answer]())
         start()
     }
-    func start() {
+    private func start() {
+        pickQuestion()
         repeat {
-            pickQuestion()
-            print(currentQuestion.title)
-            print("____________________________")
-            self.currentQuestion.answers.forEach { answer in
-                print(answer.text + "\n")
-            }
-            if let typed = readLine(strippingNewline: true) {
-                let guessNumbers = typed.split(separator: " ").map{ Int(String($0)) ?? 0 }
-                let guesses = IndexSet(guessNumbers.sorted()).map {self.currentQuestion.answers[$0]}
-                let answer = answerQuestion(using: guesses )
-                print(answer)
-                moveQuestion(when: answer)
-                print("Fresh questions: \(questions.freshCount)")
-                print("Correct questions: \(questions.correctCount)")
-                print("Mistaken questions: \(questions.mistakeCount)")
-            }
-        } while self.questions.remainingCount > 0
+            cmdQuestion()
+        } while !hasEnded()
     }
     
+    private func cmdQuestion() {
+        print(currentQuestion.title)
+        print("____________________________")
+        currentQuestion.answers.forEach { answer in
+            print(answer.text + "\n")
+        }
+        if let typed = readLine(strippingNewline: true) {
+            let guessNumbers = typed.split(separator: " ").map{ Int(String($0)) ?? 0 }
+            let guesses = IndexSet(guessNumbers.sorted()).map {currentQuestion.answers[$0]}
+            let isCorrect = answerQuestion(using: guesses )
+            print(isCorrect)
+            print("Fresh questions: \(questions.freshCount)")
+            print("Correct questions: \(questions.correctCount)")
+            print("Mistaken questions: \(questions.mistakenCount)")
+            print("Next Try In: \(nextTry ?? -1)")
+        }
+    }
     private func pickQuestion() {
-        guard var nextTry = self.nextTry else {
-            return self.currentQuestion = self.questions.fresh.randomElement()!
+        // No mistake so far
+        if mistakes == 0 {
+            return currentQuestion = questions.fresh.randomElement()!
+        }
+        if (mistakes == 1) {
+            nextTry = Int.random(in: 7...15)
         }
         if (nextTry == 0 || questions.freshCount == 0) {
-            nextTry = Int.random(in: 5...10)
-            return self.currentQuestion = self.questions.mistaken.randomElement()!
+            nextTry = Int.random(in: 7...15)
+            return currentQuestion = questions.mistaken.randomElement()!
         } else {
-            nextTry -= 1
-            return self.currentQuestion = self.questions.fresh.randomElement()!
+            nextTry! -= 1
+            return currentQuestion = questions.fresh.randomElement()!
         }
     }
     
-    private func moveQuestion(when isCorrect: Bool) {
+    private func moveQuestion(basedOn isCorrect: Bool) {
+        // Moves between question decks
         if(isCorrect) {
-            self.questions.fresh.remove(self.currentQuestion)
-            self.questions.mistaken.remove(self.currentQuestion)
-            self.questions.correct.insert(self.currentQuestion)
+            questions.fresh.remove(currentQuestion)
+            questions.mistaken.remove(currentQuestion)
+            questions.correct.insert(currentQuestion)
         } else {
-            self.questions.fresh.remove(self.currentQuestion)
-            self.questions.mistaken.insert(self.currentQuestion)
+            questions.fresh.remove(currentQuestion)
+            questions.mistaken.insert(currentQuestion)
         }
     }
     
-    func answerQuestion(using answers:[Answer]) -> Bool {
+    private func isAnswerCorrect(using answers:[Answer]) -> Bool {
         let correctAnswers = Set(currentQuestion.answers.filter {$0.isCorrect})
         return Set(answers) == correctAnswers
+    }
+    
+    func answerQuestion(using answers: [Answer]) -> Bool {
+        let isCorrect = isAnswerCorrect(using: answers)
+        if (!isCorrect) { mistakes += 1 }
+        moveQuestion(basedOn: isCorrect)
+        pickQuestion()
+        return isCorrect
     }
     
     func hasEnded() -> Bool {
@@ -155,7 +175,7 @@ class Quiz {
 }
 
 //print(cislo)
-if let questions = try? loadQuestionsFile() {
+if let questions = try? QuestionsLoader.loadQuestionsFile() {
     let quiz = Quiz(using: questions)
 }
 let cislo = readLine(strippingNewline: true)
